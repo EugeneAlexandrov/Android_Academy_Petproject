4a68d0e18849fd80a5ad80ec9e32bacff4ac6ea6
EugenAlexandrov <mybclym@gmail.com>
2020-12-02 07:55:25 +0300

    Homework#3 add interface for clicklistener

--- app/src/main/java/com/mybclym/androidacademy/petproject/FragmentMovieDetails.kt
+++ app/src/main/java/com/mybclym/androidacademy/petproject/FragmentMovieDetails.kt
@@ -1,10 +1,12 @@
 package com.mybclym.androidacademy.petproject
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 /**
  * A simple [Fragment] subclass.
@@ -12,11 +14,33 @@ import android.view.ViewGroup
  */
 class FragmentMovieDetails : Fragment() {
 
+    private var movieClickListener: MovieClickListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is MovieClickListener) {
+            movieClickListener = context
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movie_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.back_btn_tv).apply {
+            setOnClickListener {
+                movieClickListener?.showMovieList()
+            }
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        movieClickListener = null
     }
 }
--- app/src/main/java/com/mybclym/androidacademy/petproject/FragmentMoviesList.kt
+++ app/src/main/java/com/mybclym/androidacademy/petproject/FragmentMoviesList.kt
@@ -1,5 +1,6 @@
 package com.mybclym.androidacademy.petproject
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,25 +14,39 @@ import androidx.cardview.widget.CardView
  */
 class FragmentMoviesList : Fragment() {
 
-    private var movieItemCardView: CardView? = null
+    private var movieClickListener: MovieClickListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is MovieClickListener) {
+            movieClickListener = context
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movies_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        movieItemCardView = view.findViewById<CardView>(R.id.movie_item).apply {
+        view.findViewById<CardView>(R.id.movie_item).apply {
             setOnClickListener {
-                activity?.supportFragmentManager?.beginTransaction()
-                    ?.replace(R.id.fragment_container, FragmentMovieDetails())
-                    ?.addToBackStack(null)
-                    ?.commit()
+                movieClickListener?.showDetails()
             }
         }
     }
+
+    override fun onDetach() {
+        super.onDetach()
+        movieClickListener = null
+    }
 }
+
+interface MovieClickListener {
+    fun showDetails()
+    fun showMovieList()
+}
+
